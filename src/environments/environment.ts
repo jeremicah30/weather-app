57f08a922824e5e42acf6046d8cf6d4083f52cdc
jeremicah30 <maigabie17@mail.com>
2023-03-23 22:47:06 +0800

    Added Lower data and start the Http request

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  weatherApiBaseUrl: 'https://weatherbit-v1-mashape.p.rapidapi.com/current',
+  XRapidAPIHostName: 'X-RapidAPI-Host',
+  XRapidAPIHostValue: 'weatherbit-v1-mashape.p.rapidapi.com',
+  XRapidAPIKeyName: 'X-RapidAPI-Key',
+  XRapidAPIKeyValue: '1dbf642eddmshb7eceda2c5048c4p1bd296jsn94355611dae2',
+};
